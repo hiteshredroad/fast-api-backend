73a7124bee39238aae99d7c4923595c3a766db38
hitesh <hitesh.p@ciphercode.co>
2024-07-29 17:42:13 +0530

    add:request time to headers

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 import asyncio
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from contextlib import asynccontextmanager
 from fastapi import BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from routers.invoice import router as invoice_router
 from routers.auth.auth import router as auth_router
 from datetime import datetime, timedelta, timezone
+import time
 
 
 from database import invoicedb as db
@@ -54,6 +55,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+# every request milli sec
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 # Include the invoice router
